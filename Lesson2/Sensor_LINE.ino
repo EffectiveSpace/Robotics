0d19d11e3de4c759b3fb612ccfa5a56ad6a2c4c3
Alex <acmarkov7@gmail.com>
2023-03-19 13:41:42 +0300

    Урок2 + домашняя работа

---
+++ Lesson2/Sensor_LINE.ino
@@ -0,0 +1,29 @@
+int LINE_PIN = 8;
+int M1_PIN = 9;
+int M2_PIN = 10;
+
+int SPEED = 75;
+
+int sensor;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(LINE_PIN, INPUT);
+  pinMode(M1_PIN, OUTPUT);
+  pinMode(M2_PIN, OUTPUT);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  sensor = digitalRead(LINE_PIN);
+  if (sensor == 1) {
+    analogWrite(M1_PIN, 0);
+    analogWrite(M2_PIN, 0);
+  }
+  else {
+    analogWrite(M1_PIN, SPEED);
+    analogWrite(M2_PIN, SPEED);
+  }
+}
